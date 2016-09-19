af9f8d35fd883febbafd77656f8d163e1b657bce
Tomas Izo <tomas@izo.sk>
2016-09-19 03:16:30 +0200

    assignment 4, puzzle (100%)

---
+++ src/assignments/a3_puzzle/Board.java
@@ -0,0 +1,157 @@
+package assignments.a3_puzzle;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tomasizo on 9/18/16.
+ */
+
+public class Board {
+    private final int[][] blocks;
+    private final int N, hamming, manhattan;
+    private int i, j; // indices of zero, i.e. blocks[i][j] == 0;
+
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        if (blocks == null) throw new NullPointerException("called Constructor with null args");
+        N = blocks.length;
+        this.blocks = new int[N][N];
+        int ham = 0, man = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.blocks[i][j] = blocks[i][j];
+                // check for manhattan and hamming
+                int inplace = i*N + 1 + j;          // number which should be at blocks[i][j]
+                int inblock = this.blocks[i][j];    // number which is at blocks[i][j]
+                if      (inblock == 0) {this.i = i; this.j = j;}
+                else if (inblock != inplace) {
+                    ham++;
+                    man += manhattan(inblock, i, j);
+                }
+            }
+        }
+        hamming = ham;
+        manhattan = man;
+    }
+
+    // board dimension n
+    public int dimension() {
+        return N;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        return hamming;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        return manhattan;
+    }
+
+    private int manhattan(int n, int i, int j) {
+        int row = (n-1) / N;
+        int col = (n-1) % N;
+        return Math.abs(i - row) + Math.abs(j - col);
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return manhattan == 0;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        int[][] a = new int[N][N];
+        for (int k = 0; k < N; k++) {
+            for (int l = 0; l < N; l++) {
+                a[k][l] = blocks[k][l];
+            }
+        }
+        int k = Math.abs(i - 1); // choose row above or below zero
+        int l = Math.abs(j - 1); // choose col left or right from zero
+
+        //swap them
+        int t = a[k][l];
+        a[k][l] = a[k][j];
+        a[k][j] = t;
+        return new Board(a);
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        if (y == this)             return true;
+        if (!(y instanceof Board)) return false;
+        else {
+            Board that = (Board) y;
+            if (this.N != that.N || this.i != that.i || this.j != that.j
+                    ||this.hamming != that.hamming || this.manhattan != that.manhattan ) {
+                return false;
+            }
+            for (int i = 0; i < that.N; i++) {
+                for (int j = 0; j <that.N; j++) {
+                    if (this.blocks[i][j] != that.blocks[i][j]) return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        ArrayList<Board> neighbors = new ArrayList<>();
+
+        int[][] a = new int[N][N];
+        for (int k = 0; k < N; k++) {
+            for (int l = 0; l < N; l++) {
+                a[k][l] = blocks[k][l];
+            }
+        }
+
+        if (i - 1 >= 0) {
+            exch(a, i-1, j);
+            neighbors.add(new Board(a));
+            int t = a[i][j]; a[i-1][j] = t; a[i][j] = 0;
+        }
+
+        if (i + 1 < N) {
+            exch(a, i+1, j);
+            neighbors.add(new Board(a));
+            int t = a[i][j]; a[i+1][j] = t; a[i][j] = 0;
+        }
+
+        if (j - 1 >= 0) {
+            exch(a, i, j-1);
+            neighbors.add(new Board(a));
+            int t = a[i][j]; a[i][j-1] = t; a[i][j] = 0;
+        }
+
+        if (j + 1 < N) {
+            exch(a, i, j+1);
+            neighbors.add(new Board(a));
+            int t = a[i][j]; a[i][j+1] = t; a[i][j] = 0;
+        }
+        return neighbors;
+    }
+
+    // swapping any value with zero
+    private void exch(int[][] a, int row, int col) {
+        int tmp = a[row][col];
+        a[row][col] = 0;
+        a[i][j] = tmp;
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        StringBuilder sb = new StringBuilder(Integer.toString(N));
+        for (int i = 0; i < N; i++) {
+            sb.append("\n");
+            for (int j = 0; j < N; j++) {
+                sb.append(" " + blocks[i][j] + " ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/assignments/a3_puzzle/Solver.java
@@ -0,0 +1,94 @@
+package assignments.a3_puzzle;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tomasizo on 9/18/16.
+ */
+
+public class Solver {
+
+    private MinPQ<Node> orig, twin;
+    private ArrayList<Board> solution;
+    private boolean solved;
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        if (initial == null) throw new NullPointerException("null argument");
+        orig = new MinPQ<>();
+        twin = new MinPQ<>();
+        orig.insert(new Node(initial, 0, null));
+        twin.insert(new Node(initial.twin(), 0, null));
+
+        while (!solved) { // because "invalid" solution from twin is being saved too
+            solvingStep(orig);
+            solvingStep(twin);
+        }
+
+//        if (isSolvable()) {
+//            System.out.println("Minimum number of moves = " + moves() + "\n");
+//            for (Board step: solution()) System.out.println(step);
+//        }
+//       else{ System.out.println("No solution possible"); }
+    }
+
+    private void solvingStep(MinPQ<Node> heap) {
+        Node current = heap.delMin();         // delMin first
+        if (current.board.isGoal()) {         // end if goal and copy boards
+            if (heap == orig){
+                solution = new ArrayList<>();
+                while (current != null) {
+                    solution.add(0, current.board);
+                    current = current.parent;
+                }
+            }
+            solved = true;
+            return;
+        }
+
+        for (Board neighbour: current.board.neighbors()) { // add each unique neighbour
+            if (current.parent == null || !neighbour.equals(current.parent.board)){
+                heap.insert(new Node(neighbour, current.moves + 1, current));
+            }
+        }
+    }
+    private class Node implements Comparable<Node> {
+        final Board board;
+        final int moves;
+        final Node parent;
+
+        Node(Board board, int moves, Node parent) {
+            this.board = board;
+            this.moves = moves;
+            this.parent = parent;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return (this.board.manhattan() + moves) - (that.board.manhattan() + that.moves);
+        }
+    }
+
+
+    // is the initial board solvable?
+    public boolean isSolvable() {
+        return solution != null;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        if (!isSolvable()) return -1;
+        else               return solution.size() - 1;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() { return solution; }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+        int a[][] = {{0,1,3},{4,2,5},{7,8,6}};
+//        int a[][] = {{1,2,3},{4,5,6},{8,7,0}};
+        new Solver(new Board(a));
+    }
+}
